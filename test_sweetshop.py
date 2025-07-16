6ce76b84aab82c4abbd15e7c9ca27159b80a5cd5
Harsh Gohil <harshgohil.biz@gmail.com>
2025-07-16 20:06:07 +0530

    Add failing test for delete_sweet method

--- test_sweetshop.py
+++ test_sweetshop.py
@@ -2,6 +2,7 @@ import unittest
 from sweetshop import Sweet, SweetShop
 
 class TestSweetShop(unittest.TestCase):
+
     def setUp(self):
         self.shop = SweetShop()
 
@@ -9,6 +10,12 @@ class TestSweetShop(unittest.TestCase):
         sweet = Sweet(1001, "Kaju Katli", "Nut-Based", 50, 20)
         self.shop.add_sweet(sweet)
         self.assertIn(1001, self.shop.inventory)
+
+    def test_delete_sweet(self):
+        sweet = Sweet(1002, "Gulab Jamun", "Milk-Based", 10, 50)
+        self.shop.add_sweet(sweet)
+        self.shop.delete_sweet(1002)
+        self.assertNotIn(1002, self.shop.inventory)
 
 if __name__ == '__main__':
     unittest.main()
